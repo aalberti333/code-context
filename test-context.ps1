a4e47982f94286e28a2c1e85763abeb923e0491e
Anthony Alberti <31517685+aalberti333@users.noreply.github.com>
2023-11-14 08:58:54 -0700

    Update test-context.ps1

--- test-context.ps1
+++ test-context.ps1
@@ -1,40 +1,29 @@
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 
-Function Extract-ZipFile {
-    param (
-        [string]$zipFilePath,
-        [string]$extractPath
-    )
-    
-    $zipArchive = [System.IO.Compression.ZipFile]::OpenRead($zipFilePath)
-    foreach ($entry in $zipArchive.Entries) {
-        $completePath = Join-Path -Path $extractPath -ChildPath $entry.FullName
-        $directoryPath = [System.IO.Path]::GetDirectoryName($completePath)
-        
-        # Ensure the directory exists
-        if (!(Test-Path $directoryPath)) {
-            New-Item -ItemType Directory -Path $directoryPath | Out-Null
-        }
-        
-        # Extract and overwrite file
-        $fileDestinationPath = [System.IO.Path]::Combine($extractPath, $entry.FullName)
-        $fileStream = [System.IO.File]::Open($fileDestinationPath, [System.IO.FileMode]::Create)
-        try {
-            $entryStream = $entry.Open()
-            $entryStream.CopyTo($fileStream)
-        }
-        finally {
-            $fileStream.Dispose()
-            $entryStream.Dispose()
-        }
-    }
-    $zipArchive.Dispose()
-}
-
 $currentDirectory = "C:\path\to\zip" # Replace with your current directory path
+$zipFile = "$currentDirectory\file.zip" # Replace with your zip file name
 $destinationPath = "C:\extract\destination" # Replace with your destination path
-$zipFile = "file.zip" # Replace with your zip file name
+
+$tempPath = [System.IO.Path]::GetTempFileName()
+Remove-Item $tempPath # Remove the temporary file
+New-Item -ItemType Directory -Path $tempPath | Out-Null # Create a temporary directory
 
 Start-Job -ScriptBlock {
-    Extract-ZipFile -zipFilePath "$using:currentDirectory\$using:zipFile" -extractPath "$using:destinationPath"
+    try {
+        # Extract to temporary directory
+        [System.IO.Compression.ZipFile]::ExtractToDirectory($using:zipFile, $using:tempPath)
+
+        # Replace the contents of the destination path with the temp directory contents
+        Remove-Item -Path "$using:destinationPath\*" -Recurse -Force -ErrorAction SilentlyContinue
+        Move-Item -Path "$using:tempPath\*" -Destination $using:destinationPath
+
+    } catch {
+        Write-Error "An error occurred: $_"
+    } finally {
+        # Cleanup: Delete the temporary directory
+        Remove-Item -Path $using:tempPath -Recurse -Force
+    }
 }
+
+# Optionally, wait for the job to finish and output any result
+# Get-Job | Wait-Job | Receive-Job
